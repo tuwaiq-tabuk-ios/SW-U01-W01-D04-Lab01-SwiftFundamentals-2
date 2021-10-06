925e0eadbd257946c1b363308b7e962678f8967f
Ignasi Perez <perezignasi@gmail.com>
2021-10-06 09:31:31 +0300

    Functions, Variables, Constants, Types and Making decisions

---
+++ 04 Functions/Functions.playground/Pages/Infinite Loops.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+/*:
+ ## Infinite Loops
+
+ Programmers sometimes make the mistake of calling a function _from itself_. When the function is called, it calls itself, which calls itself, which calls itself...
+ 
+ This is called an _infinite loop_. It's not really infinite, because in most cases it will cause the program to run out of memory and eventually crash.
+ */
+
+func rowTheBoat() {
+    print("Row, row, row your boat")
+    print("Gently down the stream")
+}
+
+func merrilyDream() {
+    print("Merrily, merrily, merrily, merrily")
+    print("Life is but a dream")
+}
+
+func verseOne() {
+    rowTheBoat()
+    merrilyDream()
+}
+
+verseOne()
+
+/*:
+ - experiment: Make an infinite loop in the code above by editing the `verseOne` function, so it calls `verseOne()` after it calls `merrilyDream()`. Look at the console and the results sidebar. Remove the line to stop the loop. It might take a while until the playground recovers – infinite loops are hard work.
+ 
+ Next, understand how functions make working on longer programs easier to understand. 
+ 
+ [Previous](@previous)  |  page 7 of 12  |  [Next: Hiding Complexity](@next)
+*/
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # SW-U01-W01-D04-Lab01-SwiftFundamentals-2
+Fundamentsls covering functions, constants, variables, types and making decisions.
+
+## Topics
+1. Functions
+2. Constants and variables
+3. Types
+4. Making decisions
+
+  
+ ## Description
+1. Battery of exercises included in the playgrounds
+
+## DeadLine 
+Wednesday 6th October 9:15 am
